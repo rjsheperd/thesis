05a31c1e0fd42feb96704616dd41aa5467bffbbc
RJ Sheperd <rjsheperd@gmail.com>
2023-10-08 20:48:09 -0700

    Add .gitignore, NIXOS files

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ 
+    git gitRepo gnupg autoconf curl
+    procps gnumake util-linux m4 gperf unzip
+    cudatoolkit ncurses5 stdenv.cc binutils
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
